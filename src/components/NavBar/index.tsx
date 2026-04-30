29ee101e02cec08f7078c0b353cd17d4e766e2e5
Alan Garcia <alangarcia969@gmail.com>
2026-04-29 18:27:56 -0700

    setup structure

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+    return (
+        <div className="border-b-2 border-b-white">
+            NavBar
+        </div>
+    )
+}
+
+export default NavBar;
